dd33f823635d42675a03b477a4f455be5f378581
LarsBreyssens <Lars.Breyssens@student.ehb.be>
2026-05-14 18:14:51 +0200

    Css + script + JSON data teogevoegd

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+fetch("data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    const timeline = document.getElementById("timeline");
 
+    data.forEach((item) => {
+      const div = document.createElement("div");
+      div.classList.add("item");
+      div.style.left = item.position + "px";
+
+      div.innerHTML = `
+        <img src="${item.image}" alt="${item.title}">
+        <div class="info">
+          <h3>${item.title}</h3>
+          <p>${item.year}</p>
+          <p>${item.description}</p>
+        </div>
+      `;
+
+      timeline.appendChild(div);
+    });
+  });
